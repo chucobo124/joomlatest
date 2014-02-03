0c250a3c8c60a3a22b711367c4b501373852d453
mercylight <mercylight@Mercylight.(none)>
2014-02-03 23:04:42 +0800

    first_commit

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-02-02 12:47:31 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2014-02-02	12:47:31	INFO	36.239.186.66	Joomla FAILURE: 	帳號與密碼不符合，或是您還沒有註冊帳號
